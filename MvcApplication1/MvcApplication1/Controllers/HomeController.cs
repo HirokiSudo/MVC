89eb36aaef2645b6a8dded0b7cfcfdf99e99e44d
Hiroki Sudo <hiroki.sudo@basic.co.jp>
2015-10-03 00:29:12 +0900

    セッション対応済みコミット

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string returnUrl)
+        {
+            Session.Add("aaa", "bbb");
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
